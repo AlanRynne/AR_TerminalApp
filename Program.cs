bfc26dc31dedc36e13018adcbab055b57564d94e
Alan Rynne <alanrynnevidal@me.com>
2019-03-03 00:47:22 +0100

    Started dotnet console project Added AR_Lib reference Added launch and task .json

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AR_Lib;
+
+namespace AR_TerminalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
