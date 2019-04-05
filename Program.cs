25d8b99f8c38abe3fae95907c1d18b5f7e38f1cb
Alan Rynne <alanrynnevidal@me.com>
2019-04-06 01:25:13 +0200

    Added test for Matrix class Reorganized tests into methods

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using AR_Lib.Geometry;
 using AR_Lib.HalfEdgeMesh;
 using AR_Lib.IO;
 using AR_Lib.Curve;
+using AR_Lib;
 
 namespace AR_TerminalApp
 {
@@ -14,13 +15,41 @@ namespace AR_TerminalApp
         static void Main(string[] args)
         {
             string path = args[0];
+
+            TestHalfEdgeMesh(path);
+
+            TestMatrix();
+
+        }
+
+        public static void TestMatrix()
+        {
+            Console.WriteLine("---- TestMatrix() called ----");
+
+            Matrix<Point3d> matrix = new Matrix<Point3d>(2);
+            matrix[0, 0] = new Point3d(5, 6, 8);
+            matrix[0, 1] = new Point3d(3333, 6, 8);
+            matrix[1, 1] = new Point3d(5, 5.22, 8);
+            matrix[1, 0] = new Point3d(5, 6, 4.218);
+
+            Console.WriteLine("Matrix[0,0] => " + matrix[0, 0]);
+            Console.WriteLine("Matrix[0,1] => " + matrix[0, 1]);
+            Console.WriteLine("Matrix[1,0] => " + matrix[1, 0]);
+            Console.WriteLine("Matrix[1,1] => " + matrix[1, 1]);
+
+            Console.WriteLine("---- TestMatrix() ended ----");
+        }
+        public static void TestHalfEdgeMesh(string path)
+        {
+            Console.WriteLine("---- TestHalfEdgeMesh() called ----");
+
             OFFMeshData data;
             OFFResult result = OFFReader.ReadMeshFromFile(path, out data);
             Debug.WriteLine("OFFReader result: " + result + "\n");
 
 
             HE_Mesh mesh = new HE_Mesh(data.vertices, data.faces);
-            Debug.Write(mesh);
+            Debug.WriteLine(mesh);
 
             HE_MeshTopology top = new HE_MeshTopology(mesh);
             top.computeVertexAdjacency();
@@ -33,14 +62,15 @@ namespace AR_TerminalApp
             OFFResult result2 = OFFWritter.WriteMeshToFile(mesh, "/Users/alan/Desktop/AR_GeometryLibrary/AR_TerminalApp/meshes/cubeOut.off");
             Debug.WriteLine(result.ToString());
 
-            mesh.Faces[0].HalfEdge.Vertex.UserValues.Add("set1",3);
-            mesh.Faces[0].HalfEdge.Next.Vertex.UserValues.Add("set1",4);
-            mesh.Faces[0].HalfEdge.Next.Next.Vertex.UserValues.Add("set1",3);
+            mesh.Faces[0].HalfEdge.Vertex.UserValues.Add("set1", 3);
+            mesh.Faces[0].HalfEdge.Next.Vertex.UserValues.Add("set1", 4);
+            mesh.Faces[0].HalfEdge.Next.Next.Vertex.UserValues.Add("set1", 3);
 
 
             Line levelLine;
-            
-            AR_Lib.Curve.LevelSets.getFaceLevel("set1",3.5,mesh.Faces[0], out levelLine);
+            AR_Lib.Curve.LevelSets.getFaceLevel("set1", 3.5, mesh.Faces[0], out levelLine);
+
+            Console.WriteLine("---- TestHalfEdgeMesh() ended ----");
 
         }
     }
